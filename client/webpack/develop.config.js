bcce3b070be2bd8ce7df3e66358eec0c2fd9420a
Aleksey Zakharov <alexey.zakharov@soft-machine.ru>
2020-12-07 00:12:37 +0300

    Update scripts

--- client/webpack/develop.config.js
+++ client/webpack/develop.config.js
@@ -11,93 +11,89 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const package = require('../../package.json');
 
 
-module.exports = (env, argv) => {
-    const entry = ['./client/src/App.tsx'];
-
-    return ({
-        entry,
-        mode: 'development',
-        stats: {
-            logging: 'error',
-        },
-        performance: {
-            hints: false,
-        },
-        devtool: 'source-map',
-        module: {
-            rules: [{
-                test: /\.tsx?$/,
-                use: [{
-                    loader: 'ts-loader',
-                    options: {
-                        onlyCompileBundledFiles: true,
-                    },
-                }],
-                exclude: /node_modules/,
-            }, {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }, {
-                test: /\.(jpe?g|png|gif|svg)$/i,
+module.exports = (env, argv) => ({
+    entry: ['./client/src/App.tsx'],
+    mode: 'development',
+    stats: {
+        logging: 'error',
+    },
+    performance: {
+        hints: false,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    onlyCompileBundledFiles: true,
+                },
+            }],
+            exclude: /node_modules/,
+        }, {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loader: 'file-loader',
+            options: {
+                name: 'img/[name].[ext]',
+                publicPath: '/',
+            },
+        }, {
+            test: /\.(woff(2)?|eot|ttf|otf)$/,
+            use: [{
                 loader: 'file-loader',
                 options: {
-                    name: 'img/[name].[ext]',
-                    publicPath: '/',
+                    name: '[name].[ext]',
+                    outputPath: 'fonts',
+                    publicPath: '/fonts',
                 },
-            }, {
-                test: /\.(woff(2)?|eot|ttf|otf)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts',
-                        publicPath: '/fonts',
-                    },
-                }],
             }],
+        }],
+    },
+    resolve: {
+        alias: {
+            clientSrc: path.resolve(__dirname, '../src'),
         },
-        resolve: {
-            alias: {
-                clientSrc: path.resolve(__dirname, '../src'),
+        modules: ['node_modules'],
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: `client.${package.version}.[hash].js`,
+        path: path.resolve(__dirname, '../build/client'),
+        publicPath: '/',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            templateParameters: (_, outputParameters) => ({
+                jsBundle: `${outputParameters.js[0]}`,
+            }),
+            template: 'client/webpack/index.ejs',
+            inject: false,
+        }),
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
+            allowAsyncCycles: false,
+            cwd: process.cwd(),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+            analyzerPort: 3001,
+        }),
+    ],
+    devServer: {
+        port: CLIENT_STATIC_PORT,
+        historyApiFallback: true,
+        proxy: {
+            '/api': {
+                target: ['http://localhost', SERVER_PORT].join(':'),
+                secure: false,
             },
-            modules: ['node_modules'],
-            extensions: ['.tsx', '.ts', '.js'],
         },
-        output: {
-            filename: `client.${package.version}.[hash].js`,
-            path: path.resolve(__dirname, '../build/client'),
-            publicPath: '/',
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                templateParameters: (_, outputParameters) => ({
-                    jsBundle: `${outputParameters.js[0]}`,
-                }),
-                template: 'client/webpack/index.ejs',
-                inject: false,
-            }),
-            new CircularDependencyPlugin({
-                exclude: /node_modules/,
-                failOnError: true,
-                allowAsyncCycles: false,
-                cwd: process.cwd(),
-            }),
-            new BundleAnalyzerPlugin({
-                openAnalyzer: false,
-                analyzerPort: 3001,
-            }),
-        ],
-        devServer: {
-            port: CLIENT_STATIC_PORT,
-            historyApiFallback: true,
-            proxy: {
-                '/api': {
-                    target: ['http://localhost', SERVER_PORT].join(':'),
-                    secure: false,
-                },
-            },
-        },
-        watch: true,
-    });
-};
+    },
+    watch: true,
+});
